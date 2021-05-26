b68ed0ddb852eb81cb5535d99900c4a2cd4b9525
Ollie <oliver.norman1991@gmail.com>
2021-05-26 17:11:17 +0100

    correctly display live order totals

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
-  render() {
+  getTotal = (orderIds) =>
+    orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === 'available';
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
+
+  renderIndividualFish = (key) => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish.status === 'available';
+    if (!isAvailable) {
+      return (
+        <li key={key}>
+          Sorry, {fish ? fish.name : 'fish'} is no longer available!
+        </li>
+      );
+    }
     return (
-      <div className="Order">Order!</div>
-    )
+      <li key={key}>
+        {count} lbs {fish.name} {formatPrice(count * fish.price)}
+      </li>
+    );
+  };
+
+  render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = this.getTotal(orderIds);
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        <ul className="order">{orderIds.map(this.renderIndividualFish)}</ul>
+        <div className="total">
+          <strong>{formatPrice(total)}</strong>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Order
+export default Order;
